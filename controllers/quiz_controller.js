a5565dc36af59612e03b2492e00ddb1d44853707
Enrique de Guindos <eguindos@gmail.com>
2015-06-05 18:16:39 +0200

    Lista de preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -3,6 +3,41 @@
 // Importamos el modelo
 var models = require('../models/models.js');
 
+// Notar que en los dos MW primeros, usamos un find (en la bd) 
+// del Identificador que viene en req.params.quizId. El then (que 
+// sustituye al success de versiones anteriores) es lo que se 
+// hace cuando se tiene éxito en la búsqueda
+
+// GET /quizes/:quizId
+exports.show = function(req, res) {
+	models.Quiz.find(req.params.quizId).then(function(quiz) {
+		res.render('quizes/show', { quiz: quiz });
+	})
+};
+
+// GET /quizes/:quizId/answer
+exports.answer = function(req, res) {
+	models.Quiz.find(req.params.quizId).then(function(quiz) {
+		if (req.query.respuesta === quiz.respuesta) {
+			res.render('quizes/answer', {quiz: quiz, respuesta: 'Correcto'});
+		} else {
+			res.render('quizes/answer', {quiz: quiz, respuesta: 'Incorrecto'});
+		}		
+	})
+};
+
+// GET /quizes  <-- Index
+// En este caso usamo findAll()
+exports.index = function(req, res) {
+	models.Quiz.findAll().then(function(quizes) {
+		res.render('quizes/index.ejs', { quizes: quizes });
+	})
+};
+
+/*
+
+// Dejo la forma antigua para referencia
+
 // GET /quizes/question
 exports.question = function(req, res) {
 	// Buscamos la pregunta de la tabla de la BD. 
@@ -12,7 +47,8 @@ exports.question = function(req, res) {
 		res.render('quizes/question', { pregunta: quiz[0].pregunta });
 	})
 };
-
+*/
+/*
 // GET /quizes/answer
 // La respuesta llega en un GET ==> en la cabecera ==> res.query.respuesta
 exports.answer = function(req, res) {
@@ -26,3 +62,4 @@ exports.answer = function(req, res) {
 		}
 	})
 };
+*/
--- routes/index.js
+++ routes/index.js
@@ -12,8 +12,27 @@ router.get('/', function(req, res) {
 // Rutas de las preguntas y respuestas apuntando a los métodos
 // que se llamarán cuando sean esas las rutas que recibimos en un GET
 // Estos métodos se tiene que exportar de quiz_controller.js (ver arriba) 
-router.get('/quizes/question', quizController.question);
-router.get('/quizes/answer', quizController.answer);
+//
+// En versiones anteriores solo teníamos un recurso para questions y un 
+// recurso para answers, porque solo había una pregunta con su respuesta
+//
+// En versiones con DB y múltiples preguntas, vamos a tener muchos recursos
+// (en realidad un recurso por cada pregunta y otro por cada respuesta). 
+// Antes accedíamos a un recurso directamente por  /quizes/questio   o
+// /quizes/answers.
+//
+//					router.get('/quizes/question', quizController.question);
+//					router.get('/quizes/answer', quizController.answer);
+//
+// Ahora ya no. Ahora tenemos que identificar el recurso (pregunta o 
+// respuesta al que queremos acceder y darle un identificador para saber 
+// a cuál estamos accediendo exactamente). Además, para ir a la ruta 
+// principal de todas las podibles preguntas, creamos una nueva ruta 
+// que llamaremos quizes. La regEx \\d+ es para que sean números decimales. 
+// Además, index y show son nombres que se suelen dar por convenio:
+router.get('/quizes',						quizController.index); 
+router.get('/quizes/:quizId(\\d+)',			quizController.show);
+router.get('/quizes/:quizId(\\d+)/answer', 	quizController.answer);
 
 // Créditos
 router.get('/author', function(req, res) {
